90d2878ac107abfcac094199a88a819be0553ca7
italo maia <italo.maia@gmail.com>
2014-06-18 18:29:40 -0300

    fixed base test class so to use new app_factory argument format

--- examples/blog_example/tests/__init__.py
+++ examples/blog_example/tests/__init__.py
@@ -2,13 +2,14 @@
 
 from unittest import TestCase
 
+
 class BaseTestCase(TestCase):
     def setUp(self):
         import config
         from main import app_factory
         from database import create_all
 
-        self.app = app_factory(config.Testing)
+        self.app = app_factory(config.Testing, config.project_name)
         self.client = self.app.test_client()
         create_all()
 
