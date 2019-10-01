220924adf4a524b29130fbcb25b70a6da3631cbd
Arsalan <mrlethalazo@gmail.com>
2019-10-02 00:12:50 +0100

    Made pep8 compliant

--- examples/blog_example/manage.py
+++ examples/blog_example/manage.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     import config
 
     manager = script.Manager(app_factory)
-    manager.add_option("-n", "--name", dest="app_name", required=False, default=config.project_name)
-    manager.add_option("-c", "--config", dest="config", required=False, default=config.Dev)
+    manager.add_option("-n", "--name", dest="app_name", required=False,
+                       default=config.project_name)
+    manager.add_option("-c", "--config", dest="config", required=False,
+                       default=config.Dev)
     manager.add_command("test", commands.Test())
     manager.add_command("create_db", commands.CreateDB())
     manager.add_command("drop_db", commands.DropDB())
