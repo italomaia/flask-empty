85bef255c95b5aa001ede8bddb86c10b890a02a8
Italo Maia <italo.maia@gmail.com>
2020-11-10 21:46:14 +0100

    Adding app context so we can access the database

--- examples/blog_example/apps/blog/tests/utils.py
+++ examples/blog_example/apps/blog/tests/utils.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from unittest import TestCase
+from empty import Empty
 
 
 class BaseTestCase(TestCase):
@@ -9,13 +10,19 @@ class BaseTestCase(TestCase):
         from main import app_factory
         from extensions import db
 
-        self.app = app_factory(config.Test, config.project_name)
+        self.app: Empty = app_factory(config.Test, config.project_name)
         self.client = self.app.test_client()
+
+        # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xv-a-better-application-structure/page/2
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
         db.create_all()
 
     def tearDown(self):
         from extensions import db
 
+        db.session.remove()
         db.drop_all()
-        db.remove_session()
+
+        self.app_context.pop()
